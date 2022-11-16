571b31a8019d5e5bf30941827a288c86ba933812
ahoijer <hoijer.amanda@gmail.com>
2022-11-16 10:40:50 +0100

    up to date

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # NodeAuthentication
+
+Tutorial - authenticataion sessionbased application using Nodejs
+
+Branch 1-express
+Steps
+
+initial git repo, npm setup
+express, dotenv
+Use .env file to set application secrets | variables.
+
+How to run application
+
+Clone git repo branch
+Install dependencies npm i
+Start server node server
+Open a browser and visit localhost:3000
+
+### Server event order
+dependencies
+middleware | sessions, handle post
+routes | endpoints: api response json, render page using template engine
+static files
+404 not found - custom page | info
+500 server error
+listen on server requests
+
+### Template engine
+
+Embedded JavaScript templating | Handlebars
+
+https://ejs.co/
+
+*folder structur*
+
+- views
+ - partials
+  - header.ejs
+  - footer.ejs
+  - nav.ejs
+- index.ejs
+- about.ejs
+- user.ejs
+- .... // diverse olika filer, ex routes, login, register osv.
--- server.js
+++ server.js
@@ -1,26 +1,76 @@
-// dependencies - import => package.json "type": "module"
-// ====================================
-import express from 'express';
+// dependencies - import => package.json "type": "module",
+// ========================================
+import express from "express";
+import session from "express-session";
+import path from 'path';
 
 // local modules
-import { config, SITE_NAME, PORT } from './config.js'
+import { config, SITE_NAME, PORT, SESSION_SECRET, SESSION_MAXAGE } from "./configs.js";
 
-// variables 
-// ===================================
-// const PORT = process.env.PORT || 3000;
-
-// express app enviroment
-// ===================================
+// express app environment
+// ========================================
 const app = express();
 
-// routes 
-// ===================================
-app.get('/', (req, res) => {
-    res.send(`Hello World ${config.SITE_NAME}`);
+// sessions
+// ========================================
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: SESSION_MAXAGE },
+    })
+);
+
+
+// routes
+// ========================================
+
+// check sessions
+// middleware - make sure using next as 3rd argument
+app.get('*', (req, res, next) => {
+
+    // oneliner if condition - ternary operator  ? :  ;
+    req.session.views ? req.session.views++ : req.session.views = 1;
+    
+    // show number of times users navigates before session been destroyed
+    console.log("req.session.views", req.session.views);
+
+    next();
 });
 
+
+// app.get("/", (req, res) => {
+//     // res.send(`Hello world ${config.SITE_NAME}`);
+
+//     // send a file using express
+//     res.sendFile(path.resolve('./public/index.html'));
+// });
+
+// static files | folders
+// ==============================
+app.use(express.static('./public'));
+
+// 404 not found 
+// ========================================
+app.use((req, res, next) => {
+    res.status(404).send('Sry - nothing to display');
+    next();
+});
+
+// 500 server error 
+// ======================================== 
+app.use((err, req, res, next) => {
+
+    // log server error server-side
+    console.log('Error', err);
+
+    res.status(500).send('Server error - please return later');
+    next();
+})
+
 // listen on server requests
-// ===================================
+// ========================================
 app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
